a34e290e0224858f68ecbcd052d1564bdb57453a
mayuri <mayurimhjn123@gmail.com>
2023-10-07 12:07:55 +0530

    new changes made

--- src/default_package/classsnew.java
+++ src/default_package/classsnew.java
@@ -4,7 +4,7 @@ public class classsnew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("hiiiiiiiiiiiiiiiii");
+       System.out.println("hello");
 	}
 
 }
