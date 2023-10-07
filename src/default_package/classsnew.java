e15ccf3f30f7831479c526ec2606843cafa66e0d
mayuri <mayurimhjn123@gmail.com>
2023-10-07 12:14:09 +0530

    new changes are maid

--- src/default_package/classsnew.java
+++ src/default_package/classsnew.java
@@ -4,6 +4,9 @@ public class classsnew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+       System.out.println("sarthak");
+       System.out.println("mayuri");
+       System.out.println("mayuri");
        System.out.println("mayuri");
 	}
 
