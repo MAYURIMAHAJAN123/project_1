0563c342df90094d7168de06634038f8b916fb82
mayuri <mayurimhjn123@gmail.com>
2023-10-07 11:16:51 +0530

    first commit

---
+++ src/MyFirstExampleClass.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mayuri Mahajan
+ *
+ */
+public class MyFirstExampleClass {
+
+	/**
+	 * @param args
+	 * @return 
+	 */
+	public static  void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Mayuri Mahajan";
+		String name1 = "Suved Mahajan";
+		System.out.println("Hi "+name +"\n and "+name1);
+
+	}
+
+}
